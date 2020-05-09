8d98a018c15d7c70c080c0fe58776ad37345de35
Andrea Tota <andrea.tota6@gmail.com>
2020-05-09 20:08:54 +0200

    Create skeleton and style. Start logic of timer

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-alert('hello world!');
+// const counter = document.getElementById('countdown');
+let countdown;
+
+function timer(seconds) {
+  const now = Date.now();
+  const then = now + seconds * 1000;
+  displayTimeLeft(seconds);
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+    if(secondsLeft < 0) {
+      clearInterval(countdown);
+      return;
+    }
+    displayTimeLeft(secondsLeft)
+  }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+  console.log(seconds);
+}
+timer(10);
