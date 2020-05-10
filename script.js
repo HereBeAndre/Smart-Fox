e4317cd1b2d0ea3e1bfc9de270ab6f76482bc264
Andrea Tota <andrea.tota6@gmail.com>
2020-05-10 20:00:43 +0200

    Start logic for display of date

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let countdown;
 const counter = document.querySelector('.display_time_left_count');
+// const dateOfToday = document.querySelector(".date-today");
 
 function timer(seconds) {
   const now = Date.now();
@@ -18,7 +19,15 @@ function timer(seconds) {
 function displayTimeLeft(seconds) {
   const minutes = Math.floor(seconds / 60);
   const remainingSecs = seconds % 60;
-  const counterDisplay = `${minutes} : ${remainingSecs}`;
+  const counterDisplay = `${minutes} : ${remainingSecs < 10 ? "0" : ""}${remainingSecs}`;
   counter.textContent = counterDisplay;
+}
+
+function setDateOfToday() {
+  const today = new Date;
+  const todayShow = today.getDate();
+  const todayShowMonth = today.getMonth();
+  document.querySelector(".date-today").innerHTML = `${todayShow} - ${todayShowMonth}`;
+  // dateOfToday.textContent = `${todayShow} - ${todayShowMonth}`;
 }
 timer(126);
